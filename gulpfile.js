06ba80a2ab47dfd5002f8f564af76b548e9b1d99
Oseias <Oseiasanascimento@hotmail.com>
2018-07-21 18:28:21 -0300

    prefixer adicionado ao gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -6,6 +6,7 @@
 
 var gulp = require('gulp');
 var browserSync = require('browser-sync').create();
+const autoprefixer = require('gulp-autoprefixer');
 
 gulp.task('default', function() {
     //Ok
@@ -23,3 +24,12 @@ gulp.task('default', function() {
     gulp.watch("js/*.*").on('change', browserSync.reload);
     gulp.watch("css/*.*").on('change', browserSync.reload);
 });
+
+gulp.task('prefix', () =>
+    gulp.src('css/*.css')
+        .pipe(autoprefixer({
+            browsers: ['last 5 versions'],
+            cascade: false
+    }))
+    .pipe(gulp.dest('style'))
+);
