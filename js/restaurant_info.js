85c1ef7f84d339f8b433d350acd7ec61ea4e493c
Oseias <Oseiasanascimento@hotmail.com>
2018-07-21 13:56:35 -0300

    código descomentado

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -35,7 +35,7 @@ initMap = () => {
   });
 }
 
-/window.initMap = () => {
+window.initMap = () => {
   fetchRestaurantFromURL((error, restaurant) => {
     if (error) { // Got an error!
       console.error(error);
@@ -49,7 +49,7 @@ initMap = () => {
       DBHelper.mapMarkerForRestaurant(self.restaurant, self.map);
     }
   });
-} 
+}
 
 /**
  * Get current restaurant from page URL.
