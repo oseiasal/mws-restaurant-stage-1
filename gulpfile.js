19f4da30af90f0fcca366ba52465d5ebad864d99
Oseias <Oseiasanascimento@hotmail.com>
2018-07-21 12:10:20 -0300

    Tarefa BrowserSync adicionada

---
+++ gulpfile.js
@@ -0,0 +1,23 @@
+/*
+ * dar o npm init, instalar o gulp localmente, fazer o require
+ * não precisa ficar dando npm init ou instal quando instalar mó-
+ *los nas dependências.
+ */
+
+var gulp = require('gulp');
+var browserSync = require('browser-sync').create();
+
+gulp.task('default', function() {
+    //Ok
+    browserSync.init({
+        server: {
+            baseDir: './'
+        },
+        port: 8080,
+        ui: {
+            port: 8081
+        }
+    });
+
+    gulp.watch("./*.*").on('change', browserSync.reload);
+});
